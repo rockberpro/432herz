329e00528b06941bbf3e5c94586ebc723a26229f
Samuel Universo <samuel.rockenbach@universo.univates.br>
2025-01-18 20:19:39 -0300

    Fix checking undefined

--- 440hz.js
+++ 440hz.js
@@ -1,5 +1,5 @@
 stream = document.getElementsByClassName('video-stream html5-main-video')[0];
-if (stream !== 'undefined') {
+if (stream) {
     stream.preservesPitch = true;
     stream.playbackRate = 1.0;
 }
