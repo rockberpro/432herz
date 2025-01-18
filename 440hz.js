487b2ae191053dbb0dff0600a9270c9e166feb1b
Samuel Universo <samuel.rockenbach@universo.univates.br>
2025-01-18 20:23:02 -0300

    Adjust 440Hz pitch ratio

--- 440hz.js
+++ 440hz.js
@@ -1,5 +1,5 @@
 stream = document.getElementsByClassName('video-stream html5-main-video')[0];
 if (stream) {
     stream.preservesPitch = true;
-    stream.playbackRate = 1.0;
+    stream.playbackRate = 440 / 440;
 }
