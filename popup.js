e844bfe2c849b7a866d89ad9ec8e3c1879590f9d
Samuel Universo <samuel.rockenbach@universo.univates.br>
2025-01-23 00:17:21 -0300

    Drafting attempt to save storage variables for tabs

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    chrome.storage.local.get(['herzing432_pressed', 'herzing440_pressed'], function(result) {
-        if (result.herzing432_pressed) {
-            document.getElementById('herzing432').classList.add("pressed");
-        }
-        if (result.herzing440_pressed) {
-            document.getElementById('herzing440').classList.add("pressed");
-        }
-    });
+    // chrome.storage.local.get(['herzing432_pressed', 'herzing440_pressed'], function(result) {
+    //     if (result.herzing432_pressed) {
+    //         document.getElementById('herzing432').classList.add("pressed");
+    //     }
+    //     if (result.herzing440_pressed) {
+    //         document.getElementById('herzing440').classList.add("pressed");
+    //     }
+    // });
 })
 
 const herzing432 = document.getElementById('herzing432')
@@ -18,14 +18,33 @@ if (herzing432) {
                 target: { tabId: tab.id },
                 files: ["432hz.js"]
             });
+
+            
+            herzing432.classList.toggle("pressed");
+            document.getElementById('herzing440').classList.remove("pressed");
+            
+            chrome.storage.local.get('tabs', function(result) {
+                console.log(result.tabs);
+
+                index = tab.id.toString();
+
+                // if (result.tabs[index].herzing432_pressed) {
+                //     document.getElementById('herzing432').classList.add("pressed");
+                // }
+                // if (result.tabs[index].herzing440_pressed) {
+                //     document.getElementById('herzing440').classList.add("pressed");
+                // }
+
+                currentTab = {
+                    [index]: [
+                        ['herzing432_pressed', true],
+                        ['herzing440_pressed', false]
+                    ]
+                };
+                chrome.storage.local.set({tabs: currentTab});
+            });
         }
-
-        herzing432.classList.toggle("pressed");
-        document.getElementById('herzing440').classList.remove("pressed");
-
-        chrome.storage.local.set({ herzing432_pressed: true });
-        chrome.storage.local.set({ herzing440_pressed: false });
-    });    
+    });
 }
 
 const herzing440 = document.getElementById('herzing440')
@@ -37,12 +56,27 @@ if (herzing440) {
                 target: { tabId: tab.id },
                 files: ["440hz.js"]
             });
+
+            chrome.storage.local.get('tabs', function(result) {
+                console.log(result.tabs);
+
+                index = tab.id.toString();
+
+                // if (result.tabs[index].herzing432_pressed) {
+                //     document.getElementById('herzing432').classList.add("pressed");
+                // }
+                // if (result.tabs[index].herzing440_pressed) {
+                //     document.getElementById('herzing440').classList.add("pressed");
+                // }
+
+                currentTab = {
+                    [index]: [
+                        ['herzing432_pressed', false]
+                        ['herzing440_pressed', true]
+                    ]
+                };
+                chrome.storage.local.set({tabs: currentTab});
+            });
         }
-
-        herzing440.classList.toggle("pressed");
-        document.getElementById('herzing432').classList.remove("pressed");
-
-        chrome.storage.local.set({ herzing432_pressed: false });
-        chrome.storage.local.set({ herzing440_pressed: true });
-    });   
+    });
 }
