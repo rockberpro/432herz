d54870e63536197b41965a36ebb98db1b2a036ee
Samuel Universo <samuel.rockenbach@universo.univates.br>
2025-01-29 23:20:18 -0300

    Fix erros on Service Worker

--- background.js
+++ background.js
@@ -1,18 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete") {
-
-        update432HerzingTabState(tabId, false);
-        update440HerzingTabState(tabId, false);
-
-        document.getElementById('herzing432').classList.remove("pressed");
-        document.getElementById('herzing440').classList.remove("pressed");
+        chrome.storage.local.set({ [`herzing432_${tabId}`]: false }, () => {});
+        chrome.storage.local.set({ [`herzing440_${tabId}`]: false }, () => {});
     }
 });
-
-function update432HerzingTabState(tabId, pressed) {
-    chrome.storage.local.set({ [`herzing432_${tabId}`]: pressed }, () => {});
-}
-
-function update440HerzingTabState(tabId, pressed) {
-    chrome.storage.local.set({ [`herzing440_${tabId}`]: pressed }, () => {});
-}
