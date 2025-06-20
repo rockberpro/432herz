e638d0820a1dcc24b28305b10410300b303b1412
Samuel Oberger Rockenbach <samuel.rockenbach@univates.br>
2025-06-20 09:30:18 -0300

    Don't erase tab-state on reload

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete") {
-        chrome.storage.local.set({ [`herzing432_${tabId}`]: false }, () => {});
-        chrome.storage.local.set({ [`herzing440_${tabId}`]: false }, () => {});
+        /* commented so the tab state doesn't get erased on reload */
+        // chrome.storage.local.set({ [`herzing432_${tabId}`]: false }, () => {});
+        // chrome.storage.local.set({ [`herzing440_${tabId}`]: false }, () => {});
     }
 });
 
